e5e890af3481c6984f6c697a02e7544f96380070
Christoph Haene <christoph.haene@ckw.ch>
2021-11-30 09:15:56 +0100

    remove key

--- storefront/settings.py
+++ storefront/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^c4%uy$goq-^=pal_%2ru$e%4lymu7^oo4oo^7l4@n17&n$+ff'
+#SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
